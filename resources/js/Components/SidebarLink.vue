fefbc7949e137d4c5506a652dfd76d810abed043
hery_marcelo <herymarcelo@gmail.com>
2024-01-29 13:25:22 -0400

    primerVersion

---
+++ resources/js/Components/SidebarLink.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps, computed } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+  href: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+  },
+});
+
+const classes = computed(() =>
+  props.active
+    ? 'block pl-3 pr-4 py-2 bg-gray-900 text-white font-medium transition duration-300 ease-in-out'
+    : 'block pl-3 pr-4 py-2 text-gray-700 hover:bg-indigo-600 hover:text-white transition duration-300 ease-in-out'
+);
+</script>
+
+<template>
+  <Link :href="href" :class="classes">
+    <slot />
+  </Link>
+  
+</template>
+
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,48 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import SidebarLink from '@/Components/SidebarLink.vue';
+</script>
+
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <div class="flex h-screen w-full fixed">
+            <!-- Sidebar -->
+            <div class="bg-gray-800 text-white w-64 py-4">
+                <!-- Enlace para el Dashboard -->
+                <SidebarLink :href="route('dashboard')" :active="route().current('dashboard')">
+                    Dashboard
+                </SidebarLink>
+                
+                
+
+                <!-- Otros enlaces del Sidebar -->
+                <!-- <SidebarLink :href="route('example')" :active="route().current('example')">
+                    Ejemplo
+                </SidebarLink> -->
+            </div>
+
+            <!-- Contenido Principal -->
+            <div class="flex-1 p-10">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<style>
+  body,
+  html {
+    margin: 0;
+    padding: 0;
+  }
+
+  .flex {
+    margin: 0;
+  }
+</style>
